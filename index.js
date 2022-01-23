47394fca5b7ce8b7f24228c0a94d6cf9b5a302cb
‘yongliang24’ <yongliang24@gmail.com>
2022-01-23 01:34:06 -0800

    initial comment, vowel part 1 added

---
+++ index.js
@@ -0,0 +1,173 @@
+function longVowelA(){
+	let audio = new Audio('./sounds/longA.m4a'); 
+	audio.play();
+}
+
+function bake(){
+	let audio = new Audio('./sounds/Bake.m4a'); 
+	audio.play();
+}
+function lake(){
+	let audio = new Audio('./sounds/Lake.m4a'); 
+	audio.play();
+	
+}
+function cake(){
+	let audio = new Audio('./sounds/Cake.m4a'); 
+	audio.play();
+	
+}
+
+function eight(){
+	let audio = new Audio('./sounds/Eight.m4a'); 
+	audio.play();
+}
+function weight(){
+	let audio = new Audio('./sounds/Weight.m4a'); 
+	audio.play();
+	
+}
+function mail(){
+	let audio = new Audio('./sounds/Mail.m4a'); 
+	audio.play();
+	
+}
+function fail(){
+	let audio = new Audio('./sounds/Fail.m4a'); 
+	audio.play();
+}
+
+function great(){
+	let audio = new Audio('./sounds/Great.m4a'); 
+	audio.play();
+}
+function steak(){
+	let audio = new Audio('./sounds/Steak.m4a'); 
+	audio.play();
+	
+}
+function they(){
+	let audio = new Audio('./sounds/They.m4a'); 
+	audio.play();
+	
+}
+function obey(){
+	let audio = new Audio('./sounds/Obey.m4a'); 
+	audio.play();
+}
+function day(){
+	let audio = new Audio('./sounds/Day.m4a'); 
+	audio.play();
+	
+}
+function way(){
+	let audio = new Audio('./sounds/Way.m4a'); 
+	audio.play();
+}
+
+// **********************************************
+function shortA1(){
+	let audio = new Audio('./sounds/Short1.m4a'); 
+	audio.play();
+}
+
+function car(){
+	let audio = new Audio('./sounds/Car.m4a'); 
+	audio.play();
+}
+function far(){
+	let audio = new Audio('./sounds/Far.m4a'); 
+	audio.play();
+	
+}
+function father(){
+	let audio = new Audio('./sounds/Father.m4a'); 
+	audio.play();
+	
+}
+
+function party(){
+	let audio = new Audio('./sounds/Party.m4a'); 
+	audio.play();
+}
+function hard(){
+	let audio = new Audio('./sounds/Hard.m4a'); 
+	audio.play();
+	
+}
+//short2
+function shortA2(){
+	let audio = new Audio('./sounds/Short2.m4a'); 
+	audio.play();
+	
+}
+function family(){
+	let audio = new Audio('./sounds/Family.m4a'); 
+	audio.play();
+}
+
+function back(){
+	let audio = new Audio('./sounds/Back.m4a'); 
+	audio.play();
+}
+function map(){
+	let audio = new Audio('./sounds/Map.m4a'); 
+	audio.play();
+	
+}
+function man(){
+	let audio = new Audio('./sounds/Man.m4a'); 
+	audio.play();
+	
+}
+function ask(){
+	let audio = new Audio('./sounds/Ask.m4a'); 
+	audio.play();
+}
+//short3
+function shortA3(){
+	let audio = new Audio('./sounds/Short3.m4a'); 
+	audio.play();
+}
+function fall(){
+	let audio = new Audio('./sounds/Fall.m4a'); 
+	audio.play();
+}
+function water(){
+	let audio = new Audio('./sounds/Water.m4a'); 
+	audio.play();
+}
+function almost(){
+	let audio = new Audio('./sounds/Almost.m4a'); 
+	audio.play();
+}
+//short4
+function shortA4(){
+	let audio = new Audio('./sounds/Short4.m4a'); 
+	audio.play();
+	
+}
+
+function was(){
+	let audio = new Audio('./sounds/Was.m4a'); 
+	audio.play();
+}
+function what(){
+	let audio = new Audio('./sounds/What.m4a'); 
+	audio.play();
+	
+}
+function around(){
+	let audio = new Audio('./sounds/Around.m4a'); 
+	audio.play();
+	
+}
+function alone(){
+	let audio = new Audio('./sounds/Alone.m4a'); 
+	audio.play();
+}
+
+function ago(){
+	let audio = new Audio('./sounds/Ago.m4a'); 
+	audio.play();
+}
